77f2b9b1c83652712315ef81e53084eb3309b372
pythagoras-yamamoto <yamamo8533@gmail.com>
2022-05-01 23:57:41 +0900

    initialize

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,24 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_paramas)
+    redirect_to task_path
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end 
+
+  def destroy
+  end
 end
